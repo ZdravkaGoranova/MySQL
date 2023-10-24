60e4dafcdcaf597370052dc3d511eaebb42513a4
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-10-24 10:46:35 +0300

    add new exercise

--- 3.Built-in Functions/Exercises_Built-in Functions1.sql
+++ 3.Built-in Functions/Exercises_Built-in Functions1.sql
@@ -40,3 +40,70 @@ lower(job_title) not  LIKE  '%engineer%'
 ORDER BY employee_id ASC;
 
  -- 5. Find Towns with Name Length
+SELECT 
+    `name`
+FROM
+    towns
+WHERE
+    LENGTH(name) = 5 OR LENGTH(`name`) = 6
+ORDER BY name ASC;
+ 
+ -- 6. Find Towns Starting With
+SELECT 
+    *
+FROM
+    towns
+WHERE
+-- upper(left(name,1)) in ('M','K','B','E')
+-- name regexp '^[mkbe]'
+    LOWER(`name`) LIKE 'm%'
+        OR LOWER(`name`) LIKE 'k%'
+        OR LOWER(`name`) LIKE 'b%'
+	OR LOWER(`name`) LIKE 'e%'
+ORDER BY name ASC;
+
+-- 7. Find Towns Not Starting With
+SELECT 
+    *
+FROM
+    towns
+WHERE
+
+name regexp '^[^RrBbDd]'
+	-- 	upper(name) NOT LIKE 'R%'
+--         OR upper(name) NOT  LIKE 'B%'
+--         OR upper(name) NOT  LIKE 'D%'
+ORDER BY name ASC;
+
+-- 8. Create View Employees Hired After 2000 Year
+
+CREATE
+    VIEW v_employees_hired_after_2000 as
+SELECT 
+   first_name,last_name
+FROM
+    employees 
+    where year(hire_date) >2000;
+    
+    select *from v_employees_hired_after_2000;
+    
+  --  9. Length of Last Name
+SELECT 
+    first_name, last_name
+FROM
+    employees
+WHERE
+    CHAR_LENGTH(last_name) = 5;
+    
+    -- 10. Countries Holding 'A'
+    use geography;
+ 
+SELECT 
+    country_name, iso_code
+FROM
+    countries
+WHERE
+    (CHAR_LENGTH(country_name)
+    - CHAR_LENGTH(REPLACE(LOWER(country_name), 'a', '')))
+    >= 3
+ORDER BY iso_code;
