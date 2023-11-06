87e5766bee64e1e732c495100d2263def09e433c
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-11-06 10:15:28 +0200

    Add new Exercise

--- 5.Table Relations/Exercises_Table Relations1.sql
+++ 5.Table Relations/Exercises_Table Relations1.sql
@@ -213,3 +213,15 @@ CREATE TABLE agenda (
         REFERENCES subjects (subject_id)
 );
 # 07. Peaks in Rila
+
+SELECT 
+    m.mountain_range,
+    p.peak_name,
+    p.elevation AS 'peak_elevation'
+FROM
+    mountains AS m
+        JOIN
+    peaks AS p ON m.id = p.mountain_id
+WHERE
+    m.mountain_range = 'Rila'
+ORDER BY p.elevation DESC;
