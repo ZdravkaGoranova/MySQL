c09e601744c37a2e0280a0ee5a50896f73a92274
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-11-10 19:25:33 +0200

    UPDATE: add last new exercise

--- 7.Database Programmability and Transactions/Lab_Database Programmability and.sql
+++ 7.Database Programmability and Transactions/Lab_Database Programmability and.sql
@@ -96,3 +96,30 @@ WHERE
     e.employee_id = 17;
     
 # 4. Triggered
+
+DELIMITER ;
+;
+CREATE TABLE deleted_employees (
+employee_id INT PRIMARY KEY, 
+first_name VARCHAR(50),
+last_name VARCHAR(50),
+middle_name VARCHAR(50),
+job_title VARCHAR(50),
+deparment_id INT,
+salary DECIMAL(19,4)
+);
+DROP TRIGGER tr_deleted_employees;
+DROP TABLE deleted_employees;
+
+DELIMITER $$
+CREATE TRIGGER tr_deleted_employees
+AFTER DELETE
+ON employees
+FOR EACH ROW
+BEGIN
+INSERT INTO deleted_employees(employee_id,first_name,last_name,middle_name,job_title,salary)
+VALUES(OLD.employee_id,OLD.first_name,OLD.last_name,OLD.middle_name,OLD.job_title,OLD.salary);
+END$$
+DELIMITER ;
+;
+DELETE FROM employees WHERE employee_id =2;
