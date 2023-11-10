32427e66318d60eec81fd90feb36cafb7e5061c8
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-11-10 18:45:08 +0200

    Add new 1 exercise

--- 7.Database Programmability and Transactions/Lab_Database Programmability and.sql
+++ 7.Database Programmability and Transactions/Lab_Database Programmability and.sql
@@ -1,6 +1,6 @@
 # Lab: Database Programmability and Transactions
-# 1. Count Employees by Town
 
+# 1. Count Employees by Town
 SELECT 
     COUNT(*) AS 'count'
 FROM
@@ -17,8 +17,11 @@ DELIMITER $$
 CREATE FUNCTION  ufn_count_employees_by_town(town_name VARCHAR(50))
 RETURNS int
 DETERMINISTIC
-begin
-return(
+BEGIN
+-- DECLARE e_count INT;
+RETURN(
+-- set e_count:=(SELECT )
+-- return e_count;
 SELECT 
     COUNT(*) AS 'count'
 FROM
@@ -31,12 +34,38 @@ WHERE
     t.`name` = `town_name`
 GROUP BY t.town_id
 ) ;
-end $$
+END $$
+DELIMITER ;
 
-SELECT UFN_COUNT_EMPLOYEES_BY_TOWN('Edmonds') AS count
-
+SELECT UFN_COUNT_EMPLOYEES_BY_TOWN('Sofia') AS count ;
+SELECT name,UFN_COUNT_EMPLOYEES_BY_TOWN(name) AS count  from towns;
 
 # 2. Employees Promotion
+
+DELIMITER $$
+CREATE PROCEDURE usp_raise_salaries(department_name VARCHAR(50))
+BEGIN 
+
+UPDATE employees AS e 
+JOIN departments AS d 
+ON e.department_id = d.department_id 
+SET salary = salary * 1.05
+
+WHERE d.name = department_name;
+
+END$$
+DELIMITER ;
+;
+-- SET @answer=0;
+-- CALL usp_raise_salaries('Finance',@answer);
+CALL usp_raise_salaries('Finance');
+
+SELECT 
+   e.first_name, e.salary
+FROM
+    employees as e
+    where department_id=10
+    ORDER BY e.first_name, e.salary;
 
 # 3. Employees Promotion by ID
 
