14be087015c996ab52196daf20dcc6e6b0da9abd
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-11-14 15:01:10 +0200

    Add 3 and 4 Exercises

--- 8.Exercise Functions and Procedures/Database Programmability and Transactions.sql
+++ 8.Exercise Functions and Procedures/Database Programmability and Transactions.sql
@@ -32,8 +32,67 @@ DELIMITER ;
 CALL usp_get_employees_salary_above(4500);
 
 #  03. Town Names Starting With
+DELIMITER $$
+CREATE PROCEDURE usp_get_towns_starting_with(IN prefix VARCHAR(50))
+BEGIN
+SELECT 
+    name as 'town_name'
+FROM
+    towns
+WHERE
+        name LIKE CONCAT(`prefix`, '%')
+    order by `town_name` ASC;
+END $$
+DELIMITER ;
+;
+CALL usp_get_towns_starting_with('c');
+
 # 04. Employees from Town
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_from_town(IN prefix VARCHAR(50))
+BEGIN
+SELECT 
+    e.first_name, e.last_name
+FROM
+    employees AS e
+        JOIN
+    addresses AS a ON e.address_id = a.address_id
+        JOIN
+    towns AS t ON t.town_id = a.town_id
+WHERE
+    t.name = prefix
+ORDER BY e.first_name , e.last_name , e.employee_id ASC;
+END $$
+DELIMITER ;
+;
+CALL usp_get_employees_from_town('Sofia');
+
 # 05. Salary Level Function
+DELIMITER $$
+ CREATE FUNCTION ufn_get_salary_level( `empl_salary` DECIMAL (19,4))
+ RETURNS VARCHAR(20)
+DETERMINISTIC
+BEGIN
+DECLARE salary_level VARCHAR(20);
+SET salary_level = CASE 
+ WHEN `empl_salary` < 30000 THEN 'Low'
+        WHEN `empl_salary` BETWEEN 30000 AND 50000 THEN 'Average'
+        ELSE 'High'
+         END;
+		
+--           IF salary < 30000 THEN
+--         SET salary_level = 'Low';
+--     ELSEIF salary BETWEEN 30000 AND 50000 THEN
+--         SET salary_level = 'Average';
+--     ELSE
+--         SET salary_level = 'High';
+--     END IF;
+RETURN salary_level;
+END $$
+DELIMITER ;
+;
+SELECT  ufn_get_salary_level(125500.00) AS salary_Level;
+
 # 06. Employees by Salary Level
 #  07. Define Function
 # 08. Find Full Name
